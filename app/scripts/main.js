6adc205112da08b5b2a91e23b8fd2f4e696e5f30
Tommie Howze <tommie.howze@bcbsal.org>
2016-07-24 21:15:05 -0500

    mainjs changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,40 +1,57 @@
+SystemJS.config({
+  baseURL: '/scripts'
+});
+var scene, player,camera;
+
 // Get the canvas element from our HTML below
 var canvas = document.querySelector("#renderCanvas");
 // Load the BABYLON 3D engine
 var engine = new BABYLON.Engine(canvas, true);
+
+Promise.all([
+  SystemJS.import('PlayerActor.js'),
+  SystemJS.import('ArcCamera.js'),
+  SystemJS.import('WorldScene.js')
+]).then(scene = createScene());
+
+
+
+
+
 // -------------------------------------------------------------
 // Here begins a function that we will 'call' just after it's built
 var createScene = function () {
-  // Now create a basic Babylon Scene object
-  var scene = new BABYLON.Scene(engine);
-  // Change the scene background color to green.
-  scene.clearColor = new BABYLON.Color3(1, 1, 1);
 
-  var camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 1, 0.8, 10, new BABYLON.Vector3(0, 0, 0), scene);
-  camera.attachControl(canvas, false);
+
+  camera = new ArcCamera(canvas, scene);
+  player = new PlayerActor(scene);
 
   var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
 
   light.intensity = .5;
 
-  this.sphere = BABYLON.Mesh.CreateSphere("sphere1", 8, 1, scene);
-  sphere.position = new BABYLON.Vector3(0, 0, 0);
 
 
-  this.ground = BABYLON.Mesh.CreateGround("ground1", 12, 12, 2, scene);
 
-  return scene;
+  var ground = BABYLON.Mesh.CreateGround("ground1", 12, 12, 2, scene);
+
+
 };
 
 
-var scene = createScene();
 
-// Register a render loop to repeatedly render the scene
-engine.runRenderLoop(function () {
-  sphere.position.x +=.005;
-  scene.activeCamera.alpha += .01;
-  scene.render();
-});
+
+
+function startEngine(){
+  // Register a render loop to repeatedly render the scene
+  engine.runRenderLoop(function () {
+    player.position.x +=.005;
+    scene.activeCamera.alpha += .01;
+    scene.render();
+  });
+
+}
+
 
 // Watch for browser/canvas resize events
 window.addEventListener("resize", function () {
