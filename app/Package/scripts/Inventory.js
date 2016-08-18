4bb99b2300af19c27adefe605f891daa6699c697
Tommie Howze <tommie.howze@bcbsal.org>
2016-08-18 07:52:13 -0500

    Inventory creation

--- app/Package/scripts/Inventory.js
+++ app/Package/scripts/Inventory.js
@@ -1,3 +1,14 @@
 /**
  * Created by B16552 on 8/10/2016.
  */
+
+
+function Inventory(){
+
+
+  /*TODO: this function will facilitate the connection to the inventory tables on the DB
+  * just send an array of IDs and the inventory object will be returned
+  *
+  *
+  * */
+}
