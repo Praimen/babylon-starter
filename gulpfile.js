b7e2147880372f28f0737f6e0c4f9b0fcab8523e
Tommie <praimen@gmail.com>
2018-03-08 14:38:27 -0600

    working gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,7 @@
  * Created by b16552 on 2/15/2018.
  */
 var gulp = require('gulp'),
-    /*less = require('gulp-less'),*/
     webpack = require('webpack-stream'),
-    LessAutoprefix = require('less-plugin-autoprefix'),
-    autoprefix = new LessAutoprefix({browsers: ['last 2 versions']}),
     connect = require('gulp-connect');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -28,13 +25,13 @@ gulp.task('connect', function () {
 
 gulp.task('webpack', ()=> {
     // place code for your default task here
-    return gulp.src('./scripts/main.js', {allowEmpty: true})
+    return gulp.src('./app/scripts/main.js', {allowEmpty: true})
         .pipe(webpack({
             plugins: [
                 new HtmlWebpackPlugin({
-                    template: 'index.html'
+                    template: 'app/index.html'
                 }),
-                new ExtractTextPlugin("styles/main.css")
+                new ExtractTextPlugin("dist/styles/main.css")
 
             ],
             module: {
@@ -63,6 +60,8 @@ gulp.task('webpack', ()=> {
                         exclude: /(node_modules)/,
                         use: {
                             loader: 'babel-loader'
+
+
                         }
                     }
                 ]
@@ -76,4 +75,9 @@ gulp.task('webpack', ()=> {
 });
 
 gulp.task('default',
-    gulp.series('clean',gulp.parallel('webpack'), 'connect'));
+  gulp.series('clean',gulp.parallel('webpack'), 'connect')
+);
+
+gulp.task('build',
+  gulp.series('clean','webpack')
+);
