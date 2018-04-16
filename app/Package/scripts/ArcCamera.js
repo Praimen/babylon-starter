7868b743f58f878de505c276b9b7cc4a3bf3ed92
Tommie <praimen@gmail.com>
2018-04-16 16:01:33 -0600

    camera adjsutments

--- app/Package/scripts/ArcCamera.js
+++ app/Package/scripts/ArcCamera.js
@@ -8,14 +8,19 @@ export function ArcCamera(canvas,scene){
   var arcCamera = new BABYLON.FollowCamera("FollowCam", new BABYLON.Vector3(0, 10, -10), scene);
 
   //The goal distance of camera from target
+  arcCamera.radius = 20;
 
   // The goal height of camera above local origin (centre) of target
+  arcCamera.heightOffset = 15;
 
   // The goal rotation of camera around local origin (centre) of target in x y plane
+  arcCamera.rotationOffset = 90;
 
   //Acceleration of camera in moving from current to goal position
+  arcCamera.cameraAcceleration = .05;
 
   //The speed at which acceleration is halted
+  arcCamera.maxCameraSpeed = 50;
 
   //camera.target is set after the target's creation
 
